8349b2c3a07a8466914b97b7a2f26df5031af58a
Bryce Shaw <shaw.bryce@gmail.com>
2020-10-22 23:47:16 -0600

    update amplify project

--- src/lib/appSyncService.js
+++ src/lib/appSyncService.js
@@ -1,9 +1,9 @@
-import {API} from "@aws-amplify/api";
-import {graphqlOperation} from "@aws-amplify/api-graphql";
-import {listSelections} from "../graphql/queries";
-
-export const getAPIState = async () => {
-    const data = await API.graphql(graphqlOperation(listSelections))
-    console.log(data)
-    return data.data.listSelections.items;
-}
+// import {API} from "@aws-amplify/api";
+// import {graphqlOperation} from "@aws-amplify/api-graphql";
+// import {listSelections} from "../graphql/queries";
+//
+// export const getAPIState = async () => {
+//     const data = await API.graphql(graphqlOperation(listSelections))
+//     console.log(data)
+//     return data.data.listSelections.items;
+// }
